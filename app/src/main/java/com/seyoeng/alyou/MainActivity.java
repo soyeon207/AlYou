ca553e2b7df6c1b0916b85177730702dffa7f532
Se-Yeong712 <lsysy712@gmail.com>
2019-03-26 03:32:36 +0900

    Activity

--- app/src/main/java/com/seyoeng/alyou/MainActivity.java
+++ app/src/main/java/com/seyoeng/alyou/MainActivity.java
@@ -1,13 +1,39 @@
 package com.seyoeng.alyou;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button town;
+    private Button alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        town = (Button)findViewById(R.id.btn_town);
+        alarm = (Button)findViewById(R.id.btn_alarm);
+
+        town.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,town.class);
+                startActivity(intent);
+            }
+        });
+
+        alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this,alarm.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
